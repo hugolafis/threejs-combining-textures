ac64ef62c3e73ac5bd3c9f18c450412195325934
Hugo Newman <hugo.newman@aize.io>
2023-08-23 14:42:03 +0100

    Loading images async, flipping coordinates

--- src/classes/Viewer.ts
+++ src/classes/Viewer.ts
@@ -1,18 +1,45 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+const totalWidth = 512;
+const totalHeight = 512;
+
 export class Viewer {
   private camera: THREE.PerspectiveCamera;
   private controls: OrbitControls;
 
   private readonly scene = new THREE.Scene();
 
+  private texture: THREE.DataTexture;
+  private cubeTexture: THREE.CubeTexture;
+
   constructor(private readonly renderer: THREE.WebGLRenderer, private readonly canvas: HTMLCanvasElement) {
     this.initCamera();
     this.initControls();
     this.initLights();
 
-    this.initMeshes();
+    // Initialise the data texture
+    // Assuming the final tex is a pow of 2
+    const size = totalWidth * totalHeight;
+    const data = new Uint8Array(4 * size); // RGBA
+    data.fill(255); // white and full alpha
+
+    this.texture = new THREE.DataTexture(data, totalWidth, totalHeight);
+    this.texture.needsUpdate = true;
+    this.texture.colorSpace = THREE.SRGBColorSpace;
+    this.texture.magFilter = THREE.LinearFilter;
+    this.texture.minFilter = THREE.LinearFilter;
+    this.texture.flipY = true;
+    this.texture.mapping = THREE.CubeReflectionMapping;
+
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(5, 5, 5),
+      new THREE.MeshBasicMaterial({ map: this.texture, side: THREE.BackSide })
+    );
+
+    this.scene.add(cube);
+
+    this.loadTextures();
   }
 
   readonly update = (dt: number) => {
@@ -32,7 +59,7 @@ export class Viewer {
   private initCamera() {
     this.camera = new THREE.PerspectiveCamera(75, this.canvas.clientWidth / this.canvas.clientHeight);
 
-    this.camera.position.set(0, 0, 0);
+    this.camera.position.set(0, 0, 1);
 
     this.scene.add(this.camera);
   }
@@ -53,12 +80,79 @@ export class Viewer {
 
     const helper = new THREE.DirectionalLightHelper(light);
     this.scene.add(light);
-    this.scene.add(helper);
+    //this.scene.add(helper);
   }
 
-  private async initMeshes() {
+  private loadTextures() {
     const textureLoader = new THREE.TextureLoader();
 
-    //
+    const requests = [
+      {
+        position: { x: 0, y: 0 },
+        startPosition: { x: 0, y: 0 },
+        url: './assets/0_0.png',
+      },
+      {
+        position: { x: 1, y: 0 },
+        startPosition: { x: 200, y: 0 },
+        url: './assets/1_0.png',
+      },
+      {
+        position: { x: 2, y: 0 },
+        startPosition: { x: 400, y: 0 },
+        url: './assets/2_0.png',
+      },
+      {
+        position: { x: 0, y: 1 },
+        startPosition: { x: 0, y: 200 },
+        url: './assets/0_1.png',
+      },
+      {
+        position: { x: 1, y: 1 },
+        startPosition: { x: 200, y: 200 },
+        url: './assets/1_1.png',
+      },
+      {
+        position: { x: 2, y: 1 },
+        startPosition: { x: 400, y: 200 },
+        url: './assets/2_1.png',
+      },
+      {
+        position: { x: 0, y: 2 },
+        startPosition: { x: 0, y: 400 },
+        url: './assets/0_2.png',
+      },
+      {
+        position: { x: 1, y: 2 },
+        startPosition: { x: 200, y: 400 },
+        url: './assets/1_2.png',
+      },
+      {
+        position: { x: 2, y: 2 },
+        startPosition: { x: 400, y: 400 },
+        url: './assets/2_2.png',
+      },
+    ];
+
+    const position = new THREE.Vector2();
+
+    // Load the tiles async
+    requests.forEach(item => {
+      textureLoader
+        .loadAsync(item.url)
+        .then(texture => {
+          const height = texture.image.height;
+
+          position.set(item.startPosition.x, convertYCoordinate(item.startPosition.y, height));
+          this.renderer.copyTextureToTexture(position, texture, this.texture);
+        })
+        .catch(() => {
+          console.error('failed to load chunk: ', item.url);
+        });
+    });
   }
+}
+
+function convertYCoordinate(y: number, height: number) {
+  return totalHeight - (y + height);
 }
